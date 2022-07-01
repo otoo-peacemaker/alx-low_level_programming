130615f5bdb5346dd517596a52a719daa378f327
Kwesi-Welbred <infofeedacc@gmail.com>
2022-07-01 15:35:04 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game*/
+
+	int main(void){
+		
+		char lower;
+		for(lower ='a'; lower <= 'z'; lower++){
+			putchar(lower);
+			putchar('\n');
+		return (0);
+
+		}
