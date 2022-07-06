4dc13e967d931f5d901a4882b78e15beae23aab4
Kwesi-Welbred <infofeedacc@gmail.com>
2022-07-06 13:30:31 -0700

    print alphabet 10x

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  *   * Description: It prints the alphabet in lowercase fallowed by a new line
  *    * Return: Nothing.
  *     */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 		char i;
 	
