204bbe4d17364176a2d34734f016bfc168e9bdda
Kwesi-Welbred <infofeedacc@gmail.com>
2022-07-06 13:39:08 -0700

    print alphabet 10x

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,24 @@
 #include <unistd.h>
+#include "main.h"
 /**
  *  _putchar - writes the character c to stdout
  *  @c: The character to print
  *
- *  Return: On success 1.
+ *  Return: On success -1
  *  on error, -1 is returned, and errno is set appropriately.
+ *  main - Check description
+ *  Description: It prints the word Holberton, followed by a new line.
+ *  Return: 0.
  */
 
-int _putchar()
+int main(void)
 {
-	printf("_putchar \n");
+	char word[9] = "_putchar";
+	int i;
+	
+	for(i = 0; i<9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
 	return (0);
 }
