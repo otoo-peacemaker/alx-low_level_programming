129681bb031daa54c228c6d5f6f9e2445e785df3
Kwesi-Welbred <infofeedacc@gmail.com>
2022-07-07 14:33:43 -0700

    mult nums

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void printsomething(void);
-int _isupper(int c);
-int _isdigit(int c);
+int _putchar(char c);//0
+int _isupper(int c);//1
+int _isdigit(int c);//2
+int mul(int a, int b);//3
+void print_numbers(void);//4
+void print_most_numbers(void);//5
+void more_numbers(void);//6
+void print_line(int n);//7
+void print_diagonal(int n);//8
+void print_square(int size);//9
 #endif
