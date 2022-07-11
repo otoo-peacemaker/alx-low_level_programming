e924ef92a1c8b5c92ce3e50280e2d1ba696f9df2
Kwesi-Welbred <infofeedacc@gmail.com>
2022-07-11 13:17:35 -0700

    correction

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -89,10 +89,6 @@ int array[5];
   printf("%s", p);
 
 //testing for keygen: N is the Length of the password to be generated
-    int N = 10;
-  
-    // Function Call
-    randomPasswordGeneration(N);
 
 
   return (0);
