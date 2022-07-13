33f02eab2e0705a36261b5e0adb0225d245b53ad
Kwesi-Welbred <infofeedacc@gmail.com>
2022-07-13 14:31:25 -0700

    correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,17 @@
 * @src: character pointer variable
 * Return: pointer to the result sting dest
 */
-char *_strcat(char *dest, char *src)
-{
-int i, j = 0;
+	char *_strcat(char *dest, char *src)
+	{
+		int i, j = 0;
 
-for (i = 0;dest[i] != '\0'; i++)
-;
-while (src[j] != '\0')
-{
-dest[i + j] = src[j];
-j++;
-}
+		for (i = 0;dest[i] != '\0'; i++);
 
-return (dest);
-}
+		while (src[j] != '\0'){
+		dest[i + j] = src[j];
+		j++;
+		
+		}
+
+		return (dest);
+	}
