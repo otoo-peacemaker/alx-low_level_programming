51f7302d12fa7036318e52d34930cc8797869959
Peacemaker Otoo <infofeedacc@gmail.com>
2022-07-20 21:17:19 +0000

    betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- *_puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: An input string to printing
+ * Return: Nothing
  */
 
 void _puts_recursion(char *s)
