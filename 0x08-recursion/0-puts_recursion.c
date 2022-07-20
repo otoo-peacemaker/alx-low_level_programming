9f95bd73b7cf7b7bb4c1f7c31993fe8f3b3167bb
Peacemaker Otoo <infofeedacc@gmail.com>
2022-07-20 21:16:10 +0000

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
+ *_puts_recursion - prints a string, followed by a new line
  * @s: string to print
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
