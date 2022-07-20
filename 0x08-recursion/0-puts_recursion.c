bcae27bfb974723aac19b3305d83b95592ce1fae
Peacemaker Otoo <infofeedacc@gmail.com>
2022-07-20 21:19:11 +0000

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,9 @@
 
 /**
  * _puts_recursion - a function that prints a string, followed by a new line.
- * @s: An input string to printing
+ *
+ *@s: An input string to printing
+ *
  * Return: Nothing
  */
 
